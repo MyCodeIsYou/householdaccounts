4f03e4b82e4335132b22c2b404533c880841346f
isITinnovation <insonggod@gmail.com>
2026-04-13 16:55:16 +0900

    앱 글자 크기 고정

--- android/app/src/main/java/com/isit/householdaccounts/MainActivity.java
+++ android/app/src/main/java/com/isit/householdaccounts/MainActivity.java
@@ -1,5 +1,16 @@
 package com.isit.householdaccounts;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Android 시스템 글꼴 크기 설정이 WebView에 영향을 주지 않도록 고정
+        WebSettings webSettings = this.bridge.getWebView().getSettings();
+        webSettings.setTextZoom(100);
+    }
+}
